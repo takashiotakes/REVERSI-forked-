59a3b4888e2d6940674ef0ba334dbb27d66b2b11
takashi otake <takashiotakes@gmail.com>
2025-06-21 16:21:08 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OthelloGame from "./OthelloGame";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <OthelloGame />
+    </div>
+  );
+};
+
+export default App;
